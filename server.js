8022887690650bfec6c706d6715a1c7b67184a06
JOHN <gualterosjohn40@gmail.com>
2022-10-25 09:36:46 -0500

    connection database

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const dotenv = require('dotenv')
 const bootcampRoutes = require('./routes/bootcampRoutes.js')
 const userRoutes = require('./routes/userRoutes')
 const app = express();
-
+const connection =  require('./config/db')
 dotenv.config({
     path: './config_env/config.env'
 })
-
+// connection()
 app.use('/api/v1', bootcampRoutes)
 app.use('/api/v1/users', userRoutes)
 
